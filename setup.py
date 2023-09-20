4618456bea4d5301f798ae715a0fc04db89ba5cb
Clément Sicard <clement.sicard@icloud.com>
2023-09-20 11:20:15 -0400

    setup.py: adds missing dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 import setuptools
 
 setuptools.setup(
-    name='audio_slowfast',
-    version='0.0.1',
-    description='Slow-Fast Auditory Streams For Audio Recognition',
-    long_description=open('README.md').read().strip(),
-    long_description_content_type='text/markdown',
-    author='Evangelos Kazakos',
-    url=f'https://github.com/beasteers/auditory-slow-fast',
+    name="audio_slowfast",
+    version="0.0.1",
+    description="Slow-Fast Auditory Streams For Audio Recognition",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/markdown",
+    author="Evangelos Kazakos",
+    url=f"https://github.com/beasteers/auditory-slow-fast",
     packages=setuptools.find_packages(),
     install_requires=[
-        'torch', 'librosa', 'h5py', 'fvcore', 'simplejson', 'psutil',
+        "torch",
+        "librosa",
+        "h5py",
+        "wandb",
+        "fvcore",
+        "simplejson",
+        "psutil",
+        "tensorboard",
     ],
     extras_require={
-        'test': ['pytest', 'pytest-cov'],
-        'doc': ['sphinx-rtd-theme'],
+        "test": ["pytest", "pytest-cov"],
+        "doc": ["sphinx-rtd-theme"],
     },
-    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.',
-    keywords='auditory slowfast audio recognition')
+    license="Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.",
+    keywords="auditory slowfast audio recognition",
+)
