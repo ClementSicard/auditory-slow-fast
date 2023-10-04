5d8039f6321b2dfb76a301156e618f0654af4577
Clément Sicard <clement.sicard@icloud.com>
2023-10-04 16:07:21 -0400

    removes CustomResNetBasiHead from imports

--- audio_slowfast/__init__.py
+++ audio_slowfast/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from .audio_slowfast import AudioSlowFast  # noqa
-from .custom_resnet_head import CustomResNetBasicHead  # noqa
 from .tools.test_net import test  # noqa
 from .tools.train_net import train  # noqa
 from .utils.env import setup_environment
