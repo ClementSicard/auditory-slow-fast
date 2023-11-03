a9e46f0ab170d04571cdc4fdf96cc1b8c46ae69e
Clément Sicard <clement.sicard@icloud.com>
2023-11-03 15:58:42 -0400

    gpu: adds dummy script to increase GPU usage

---
+++ fix_weights.py
@@ -0,0 +1,41 @@
+from collections import OrderedDict
+from typing import Any
+import torch
+from loguru import logger
+import os
+
+from audio_slowfast.config.defaults import get_cfg
+from audio_slowfast.audio_slowfast import AudioSlowFast
+
+
+def main() -> None:
+    checkpoint = load_checkpoint("models/asf/weights/SLOWFAST_EPIC.pyth")
+
+    cfg = get_cfg()
+    cfg.merge_from_file("./models/asf/config/SLOWFAST_R50.yaml")
+
+    cp_model_state_new = OrderedDict()
+    for k in checkpoint["model_state"].keys():
+        logger.info(f"Fixing weights for {k}")
+        cp_model_state_new[f"model.{k}"] = checkpoint["model_state"][k]
+
+    checkpoint["model_state"] = cp_model_state_new
+
+    logger.debug(f"{checkpoint['model_state'].keys()}")
+    with open("models/asf/weights/SLOWFAST_EPIC_fixed.pyth", "wb") as f:
+        torch.save(checkpoint, f)
+
+
+def load_checkpoint(path: str) -> OrderedDict[str, Any]:
+    if not os.path.exists(path=path):
+        logger.error(f"Checkpoint '{path}' does not exist")
+        exit(1)
+
+    with open(path, "rb") as f:
+        checkpoint = torch.load(f, map_location="cpu")
+
+    return checkpoint
+
+
+if __name__ == "__main__":
+    main()
---
+++ gpu.py
@@ -0,0 +1,18 @@
+from loguru import logger
+import torch
+import time
+
+
+def run():
+    # Create a tensor that will take 5 Go of memory
+    x = torch.randn(100000000, 10).cuda()
+
+    while True:
+        x = x * 2
+        x = x / 2
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    logger.info("Starting GPU stress test")
+    run()
