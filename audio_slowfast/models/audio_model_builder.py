33a91fbb9747b228bb92451d4e99dfad0d3a0465
Clément Sicard <clement.sicard@icloud.com>
2023-11-16 12:04:33 -0500

    audio_model_builder: adds AudioSlowFastGRU

--- audio_slowfast/models/audio_model_builder.py
+++ audio_slowfast/models/audio_model_builder.py
@@ -9,6 +9,7 @@ from loguru import logger
 
 import audio_slowfast.utils.weight_init_helper as init_helper
 from audio_slowfast.models.batchnorm_helper import get_norm
+from audio_slowfast.custom_resnet_head import GRUResNetBasicHead
 
 from . import head_helper, resnet_helper, stem_helper
 from .build import MODEL_REGISTRY
@@ -286,6 +287,239 @@ class SlowFast(nn.Module):
         )
 
         self.head = head_helper.ResNetBasicHead(
+            dim_in=[
+                width_per_group * 32,
+                width_per_group * 32 // cfg.SLOWFAST.BETA_INV,
+            ],
+            num_classes=cfg.MODEL.NUM_CLASSES if len(cfg.MODEL.NUM_CLASSES) > 1 else cfg.MODEL.NUM_CLASSES[0],
+            pool_size=[
+                [
+                    cfg.AUDIO_DATA.NUM_FRAMES // cfg.SLOWFAST.ALPHA // 4 // pool_size[0][0],
+                    cfg.AUDIO_DATA.NUM_FREQUENCIES // 32 // pool_size[0][1],
+                ],
+                [
+                    cfg.AUDIO_DATA.NUM_FRAMES // 4 // pool_size[1][0],
+                    cfg.AUDIO_DATA.NUM_FREQUENCIES // 32 // pool_size[1][1],
+                ],
+            ],
+            dropout_rate=cfg.MODEL.DROPOUT_RATE,
+            act_func=cfg.MODEL.HEAD_ACT,
+        )
+
+    def forward(self, x, bboxes=None):
+        x = self.s1(x)
+        x = self.s1_fuse(x)
+        x = self.s2(x)
+        x = self.s2_fuse(x)
+        for pathway in range(self.num_pathways):
+            pool = getattr(self, "pathway{}_pool".format(pathway))
+            x[pathway] = pool(x[pathway])
+        x = self.s3(x)
+        x = self.s3_fuse(x)
+        x = self.s4(x)
+        x = self.s4_fuse(x)
+        x = self.s5(x)
+        x = self.head(x)
+        return x
+
+    def freeze_fn(self, freeze_mode):
+        if freeze_mode == "bn_parameters":
+            print("Freezing all BN layers' parameters except the first one.")
+            for n, m in self.named_modules():
+                if (isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.SyncBatchNorm)) and (
+                    "s1.pathway0_stem.bn" not in n and "s1.pathway1_stem.bn" not in n and "s1_fuse.bn" not in n
+                ):
+                    # shutdown parameters update in frozen mode
+                    m.weight.requires_grad_(False)
+                    m.bias.requires_grad_(False)
+        elif freeze_mode == "bn_statistics":
+            print("Freezing all BN layers' statistics except the first one.")
+            for n, m in self.named_modules():
+                if (isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.SyncBatchNorm)) and (
+                    "s1.pathway0_stem.bn" not in n and "s1.pathway1_stem.bn" not in n and "s1_fuse.bn" not in n
+                ):
+                    # shutdown running statistics update in frozen mode
+                    m.eval()
+
+
+@MODEL_REGISTRY.register()
+class AudioSlowFastGRU(nn.Module):
+    """
+    SlowFast model builder for SlowFast network.
+
+    Evangelos Kazakos, Arsha Nagrani, Andrew Zisserman, Dima Damen.
+    "Auditory Slow-Fast Networks for Audio Recognition"
+
+    Christoph Feichtenhofer, Haoqi Fan, Jitendra Malik, and Kaiming He.
+    "SlowFast networks for video recognition."
+    https://arxiv.org/pdf/1812.03982.pdf
+    """
+
+    def __init__(self, cfg):
+        """
+        The `__init__` method of any subclass should also contain these
+            arguments.
+        Args:
+            cfg (CfgNode): model building configs, details are in the
+                comments of the config file.
+        """
+        super(AudioSlowFastGRU, self).__init__()
+        self.norm_module = get_norm(cfg)
+        self.num_pathways = 2
+        self._construct_network(cfg)
+        init_helper.init_weights(self, cfg.MODEL.FC_INIT_STD, cfg.RESNET.ZERO_INIT_FINAL_BN)
+
+    def _construct_network(self, cfg):
+        """
+        Builds a SlowFast model. The first pathway is the Slow pathway and the
+            second pathway is the Fast pathway.
+
+        Args:
+            cfg (CfgNode): model building configs, details are in the
+                comments of the config file.
+        """
+        assert cfg.MODEL.ARCH in _POOL1.keys()
+        pool_size = _POOL1[cfg.MODEL.ARCH]
+        assert len({len(pool_size), self.num_pathways}) == 1
+        assert cfg.RESNET.DEPTH in _MODEL_STAGE_DEPTH.keys()
+
+        (d2, d3, d4, d5) = _MODEL_STAGE_DEPTH[cfg.RESNET.DEPTH]
+
+        num_groups = cfg.RESNET.NUM_GROUPS
+        width_per_group = cfg.RESNET.WIDTH_PER_GROUP
+        dim_inner = num_groups * width_per_group
+        out_dim_ratio = cfg.SLOWFAST.BETA_INV // cfg.SLOWFAST.FUSION_CONV_CHANNEL_RATIO
+
+        temp_kernel = _TEMPORAL_KERNEL_BASIS[cfg.MODEL.ARCH]
+
+        self.s1 = stem_helper.AudioModelStem(
+            dim_in=cfg.DATA.INPUT_CHANNEL_NUM,
+            dim_out=[width_per_group, width_per_group // cfg.SLOWFAST.BETA_INV],
+            kernel=[temp_kernel[0][0] + [7], temp_kernel[0][1] + [7]],
+            stride=[[2, 2]] * 2,
+            padding=[
+                [temp_kernel[0][0][0] // 2, 3],
+                [temp_kernel[0][1][0] // 2, 3],
+            ],
+            norm_module=self.norm_module,
+        )
+        self.s1_fuse = FuseFastToSlow(
+            width_per_group // cfg.SLOWFAST.BETA_INV,
+            cfg.SLOWFAST.FUSION_CONV_CHANNEL_RATIO,
+            cfg.SLOWFAST.FUSION_KERNEL_SZ,
+            cfg.SLOWFAST.ALPHA,
+            norm_module=self.norm_module,
+        )
+        self.s2 = resnet_helper.ResStage(
+            dim_in=[
+                width_per_group + width_per_group // out_dim_ratio,
+                width_per_group // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_out=[
+                width_per_group * 4,
+                width_per_group * 4 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_inner=[dim_inner, dim_inner // cfg.SLOWFAST.BETA_INV],
+            temp_kernel_sizes=temp_kernel[1],
+            stride=cfg.RESNET.FREQUENCY_STRIDES[0],
+            num_blocks=[d2] * 2,
+            num_groups=[num_groups] * 2,
+            num_block_temp_kernel=cfg.RESNET.NUM_BLOCK_TEMP_KERNEL[0],
+            trans_func_name=cfg.RESNET.TRANS_FUNC,
+            dilation=cfg.RESNET.FREQUENCY_DILATIONS[0],
+            norm_module=self.norm_module,
+        )
+        self.s2_fuse = FuseFastToSlow(
+            width_per_group * 4 // cfg.SLOWFAST.BETA_INV,
+            cfg.SLOWFAST.FUSION_CONV_CHANNEL_RATIO,
+            cfg.SLOWFAST.FUSION_KERNEL_SZ,
+            cfg.SLOWFAST.ALPHA,
+            norm_module=self.norm_module,
+        )
+
+        for pathway in range(self.num_pathways):
+            pool = nn.MaxPool2d(
+                kernel_size=pool_size[pathway],
+                stride=pool_size[pathway],
+                padding=[0, 0],
+            )
+            self.add_module("pathway{}_pool".format(pathway), pool)
+
+        self.s3 = resnet_helper.ResStage(
+            dim_in=[
+                width_per_group * 4 + width_per_group * 4 // out_dim_ratio,
+                width_per_group * 4 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_out=[
+                width_per_group * 8,
+                width_per_group * 8 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_inner=[dim_inner * 2, dim_inner * 2 // cfg.SLOWFAST.BETA_INV],
+            temp_kernel_sizes=temp_kernel[2],
+            stride=cfg.RESNET.FREQUENCY_STRIDES[1],
+            num_blocks=[d3] * 2,
+            num_groups=[num_groups] * 2,
+            num_block_temp_kernel=cfg.RESNET.NUM_BLOCK_TEMP_KERNEL[1],
+            trans_func_name=cfg.RESNET.TRANS_FUNC,
+            dilation=cfg.RESNET.FREQUENCY_DILATIONS[1],
+            norm_module=self.norm_module,
+        )
+        self.s3_fuse = FuseFastToSlow(
+            width_per_group * 8 // cfg.SLOWFAST.BETA_INV,
+            cfg.SLOWFAST.FUSION_CONV_CHANNEL_RATIO,
+            cfg.SLOWFAST.FUSION_KERNEL_SZ,
+            cfg.SLOWFAST.ALPHA,
+            norm_module=self.norm_module,
+        )
+
+        self.s4 = resnet_helper.ResStage(
+            dim_in=[
+                width_per_group * 8 + width_per_group * 8 // out_dim_ratio,
+                width_per_group * 8 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_out=[
+                width_per_group * 16,
+                width_per_group * 16 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_inner=[dim_inner * 4, dim_inner * 4 // cfg.SLOWFAST.BETA_INV],
+            temp_kernel_sizes=temp_kernel[3],
+            stride=cfg.RESNET.FREQUENCY_STRIDES[2],
+            num_blocks=[d4] * 2,
+            num_groups=[num_groups] * 2,
+            num_block_temp_kernel=cfg.RESNET.NUM_BLOCK_TEMP_KERNEL[2],
+            trans_func_name=cfg.RESNET.TRANS_FUNC,
+            dilation=cfg.RESNET.FREQUENCY_DILATIONS[2],
+            norm_module=self.norm_module,
+        )
+        self.s4_fuse = FuseFastToSlow(
+            width_per_group * 16 // cfg.SLOWFAST.BETA_INV,
+            cfg.SLOWFAST.FUSION_CONV_CHANNEL_RATIO,
+            cfg.SLOWFAST.FUSION_KERNEL_SZ,
+            cfg.SLOWFAST.ALPHA,
+            norm_module=self.norm_module,
+        )
+
+        self.s5 = resnet_helper.ResStage(
+            dim_in=[
+                width_per_group * 16 + width_per_group * 16 // out_dim_ratio,
+                width_per_group * 16 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_out=[
+                width_per_group * 32,
+                width_per_group * 32 // cfg.SLOWFAST.BETA_INV,
+            ],
+            dim_inner=[dim_inner * 8, dim_inner * 8 // cfg.SLOWFAST.BETA_INV],
+            temp_kernel_sizes=temp_kernel[4],
+            stride=cfg.RESNET.FREQUENCY_STRIDES[3],
+            num_blocks=[d5] * 2,
+            num_groups=[num_groups] * 2,
+            num_block_temp_kernel=cfg.RESNET.NUM_BLOCK_TEMP_KERNEL[3],
+            trans_func_name=cfg.RESNET.TRANS_FUNC,
+            dilation=cfg.RESNET.FREQUENCY_DILATIONS[3],
+            norm_module=self.norm_module,
+        )
+
+        self.head = GRUResNetBasicHead(
             dim_in=[
                 width_per_group * 32,
                 width_per_group * 32 // cfg.SLOWFAST.BETA_INV,
