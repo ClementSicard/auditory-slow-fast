loss: adds tests + fixes masking

audio_slowfast/models/audio_model_builder.py
@@ -3,6 +3,7 @@
 
 """Audio models."""
 
+from typing import Optional
 import torch
 import torch.nn as nn
 import pandas as pd
@@ -554,7 +555,7 @@ class AudioSlowFastGRU(nn.Module):
         self,
         x: torch.Tensor,
         lengths: torch.Tensor,
-        noun_embeddings: torch.Tensor,
+        noun_embeddings: Optional[torch.Tensor] = None,
     ):
         """
         Similar to the original `forward()` method in AudioSlowFast, but with the addition of the noun embeddings.
@@ -586,11 +587,12 @@ class AudioSlowFastGRU(nn.Module):
         x = self.s4(x)
         x = self.s4_fuse(x)
         x = self.s5(x)
+
         x = self.head(
             x,
-            noun_embeddings=noun_embeddings,
             initial_batch_shape=(B, N),
             lengths=lengths,
+            noun_embeddings=noun_embeddings,
         )
         return x
 

audio_slowfast/models/gru_head_helper.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from loguru import logger
 
 import torch
@@ -102,9 +102,9 @@ class GRUResNetBasicHead(nn.Module):
     def forward(
         self,
         inputs: torch.Tensor,
-        noun_embeddings: torch.Tensor,
         lengths: List[int],
         initial_batch_shape: torch.Size,
+        noun_embeddings: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         """
         Forward function for the GRU ResNet head. It first passes the spectrograms embeddings through the GRU to output

audio_slowfast/utils/loss.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 
 class MaskedLoss(nn.Module):
-    def __init__(self, reduction: str = "mean"):
+    def __init__(self):
         super().__init__()
         self.mse = nn.MSELoss(reduction="none")
         self.bce = nn.BCELoss(reduction="none")
@@ -18,11 +18,11 @@ class MaskedLoss(nn.Module):
 
         preds shape: (B, N, 12), labels shape: (B, N, 12)
         """
-        indices_to_discard = labels == -10
+        indices_to_keep = labels != -10
 
         # Create mask for the loss
-        mask = torch.ones_like(labels, dtype=torch.bool)
-        mask[indices_to_discard] = False
+        mask = torch.zeros_like(labels, dtype=torch.bool)
+        mask[indices_to_keep] = True
 
         abs_preds = torch.abs(preds)
         abs_labels = torch.abs(labels)
@@ -31,11 +31,14 @@ class MaskedLoss(nn.Module):
         assert torch.all(abs_preds[mask] <= 1), "abs_preds not in range [0, 1]"
 
         # Get the indices where the abs_labels are 1
-        pos_mask_indices = abs_labels.nonzero(as_tuple=True)
+        # pos_mask_indices = abs_labels.nonzero(as_tuple=True)
+        pos_mask_indices = torch.zeros_like(abs_labels, dtype=torch.bool)
+        pos_mask_indices[abs_labels == 1] = True
+        pos_mask_indices *= mask
 
         bce_term = self.bce(abs_preds[mask], abs_labels[mask])
         mse_term = self.mse(preds[pos_mask_indices], labels[pos_mask_indices])
 
-        result = bce_term.mean() + mse_term.mean()
+        result = 0.5 * (bce_term.mean() + mse_term.mean())
 
         return result